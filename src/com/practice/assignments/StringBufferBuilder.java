4b182900b74df4e5cf071b5e117c4e398b7bc454
ajs-108 <ajs@narola.email>
2025-01-21 09:44:45 +0530

    Updated String task

--- src/com/practice/assignments/StringBufferBuilder.java
+++ src/com/practice/assignments/StringBufferBuilder.java
@@ -1,7 +1,5 @@
 package com.practice.assignments;
 
-import static java.lang.Thread.sleep;
-
 public class StringBufferBuilder {
     public static void main(String[] args) throws InterruptedException {
 
@@ -17,13 +15,49 @@ public class StringBufferBuilder {
         System.out.println(str3 == str4);//false
 
         //When you need a mutable string use StringBuilder/StringBuffer, i.e. when you need to change value of string without reassigning
-        //
-        StringBuffer str5 = new StringBuffer("Arpan");
+        StringBuffer str5 = new StringBuffer("HI");
         concat1(str1);
         concat2(str5);
         System.out.println(str1);//Hello
-        System.out.println(str5);//
+        System.out.println(str5);//Hi Hello
 
+        //Below code shows the difference between StringBuilder and StringBuffer through the threads
+        //In case of StringBuffer as it is synchronized the values are printed correctly
+        //But in case of StringBuilder it prints inconsistent or corrupted values
+
+        // StringBuilder (not synchronized) task --> printing values from 0 to 39
+        StringBuilder stringBuilder = new StringBuilder("");
+        Runnable task1 = () -> {
+            for (int i = 0; i < 40; i++) {
+                stringBuilder.append(i + " ");
+            }
+            System.out.println("StringBuilder: " + stringBuilder.toString());
+        };
+
+        // StringBuffer (synchronized)
+        StringBuffer stringBuffer = new StringBuffer("");
+        Runnable task2 = () -> {
+            for (int i = 0; i < 40; i++) {
+                stringBuffer.append(i + " ");
+            }
+            System.out.println("StringBuffer: " + stringBuffer.toString());
+        };
+
+        // Creating threads for StringBuilder; Assigning same task to two threads to find out the difference
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task1);
+
+        // Creating threads for StringBuffer
+        Thread thread3 = new Thread(task2);
+        Thread thread4 = new Thread(task2);
+
+        // Starting threads for StringBuilder
+        thread1.start();
+        thread2.start();
+
+        // Starting threads for StringBuffer
+        thread3.start();
+        thread4.start();
     }
 
     public static void concat1(String s) {
@@ -32,7 +66,8 @@ public class StringBufferBuilder {
     }
 
     public static void concat2(StringBuffer s) {
-        //same for StringBuffer
-        s.append(" Shah");
+
+        s.append(" Hello");
     }
+
 }
