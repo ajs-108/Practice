8ba48c99464babb2b011cb4b0a963a1d6e5d479b
ajs-108 <ajs@narola.email>
2025-01-21 09:46:16 +0530

    Practiced with Wrapper and Set

--- src/com/practice/collection/SetExample.java
+++ src/com/practice/collection/SetExample.java
@@ -2,12 +2,13 @@ package com.practice.collection;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetExample {
 
     public static void main(String[] args) {
 
-        Set<Integer> numSet1 = new HashSet<>();
+        HashSet<Integer> numSet1 = new HashSet<>();
         numSet1.add(4);
         numSet1.add(2);
         numSet1.add(1);
@@ -16,9 +17,22 @@ public class SetExample {
         numSet1.add(3);
         numSet1.add(6);
 
-        System.out.println(numSet1);
+        System.out.println("HashSet example: "+numSet1);
         System.out.println(numSet1.contains(4));
         numSet1.remove(3);
-        System.out.println(numSet1);
+        System.out.println("HashSet after removing element 3(obj):"+numSet1);
+
+        //Sorts the set and maintains the ascending order
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(1);
+        treeSet.add(4);
+        treeSet.add(2);
+        treeSet.add(7);
+        treeSet.add(3);
+        treeSet.add(6);
+        treeSet.add(5);
+
+        System.out.println("TreeSet example: "+treeSet);
+        System.out.println(treeSet.headSet(5));
     }
 }
--- src/com/practice/other/Wrapper.java
+++ src/com/practice/other/Wrapper.java
@@ -1,2 +1,18 @@
-package com.practice.other;public class Wrapper {
+package com.practice.other;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+
+        Integer i = 127;
+        Integer j = 127;
+
+        Integer k = 128;
+        Integer l = 128;
+        System.out.println(i==j);//true
+        //Reason: Both are pointing to same variable
+        System.out.println(k==l);//false
+        //Reason: Integer has range from -128 to 127, outside this range every variable is assigned a new memory
+
+    }
 }
