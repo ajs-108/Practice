6b3dfdf1a50398974bfa205e0cd56d3610ac518a
ajs-108 <ajs@narola.email>
2025-02-03 11:55:35 +0530

    Updated Assignment 7
    changes in printing statements

--- src/com/practice/assignments/java8/stream/Assignment7.java
+++ src/com/practice/assignments/java8/stream/Assignment7.java
@@ -13,14 +13,24 @@ public class Assignment7 {
 
     public static void main(String[] args) {
 
-        List<Integer> num = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        //defining the string with initial values
+        List<String> str = new ArrayList<>(Arrays.asList("Arpan", "on", "this", "there", "Which"));
 
-        Spliterator<Integer> split1 = num.spliterator();
+        //creating a spliterator instance
+        Spliterator<String> split1 = str.spliterator();
 
-        Spliterator<Integer> split2 = split1.trySplit();
+        //trying to split the split instance created
+        Spliterator<String> split2 = split1.trySplit();
 
+        //Displaying yhe size of split parts
+        System.out.println("Size of first split: "+split1.estimateSize());
+        System.out.println("Size of second split: "+split2.estimateSize());
+
+        //printing the elements of splits
+        System.out.println("Elements of first split: ");
         split1.forEachRemaining(System.out::println);
         System.out.println();
+        System.out.println("Elements of second split: ");
         split2.forEachRemaining(System.out::println);
 
     }
